03632d88c203159cb93f6ab1c449cc86c87c5c01
Harshita <chandaliyaharshita222@gmail.com>
2023-04-12 18:21:30 +0530

    Repeat Till Wining Position

--- Snake And Ladder/Program.cs
+++ Snake And Ladder/Program.cs
@@ -11,29 +11,37 @@ namespace Snake_And_Ladder
             Console.WriteLine(" Position = " + Player_position);
             Console.WriteLine(" The Player rolls the die to get a number between 1 to 6");
             Random random = new Random();
-            int Die_num = random.Next(1, 7);
-            Console.WriteLine(" Number after rolling a die:");
-            Console.WriteLine($" {Die_num}");
 
-            int option = random.Next(3);
-            switch (option)
+            while (Player_position <= 100)
             {
-                case 0:
-                    Console.WriteLine("No Play");
-                    Console.WriteLine("The Position is " + Player_position);
-                    break;
-                case 1:
-                    Console.WriteLine("Ladder");
-                    Player_position = Player_position + Die_num;
-                    Console.WriteLine("The Position is " + Player_position);
-                    break;
-                case 2:
-                    Console.WriteLine("Snake");
-                    Player_position = Player_position - Die_num;
-                    Console.WriteLine("The Position is " + Player_position);
-                    break;
-            }
+                int Die_num = random.Next(1, 7);
+                Console.WriteLine(" Number after rolling a die:");
+                Console.WriteLine($" {Die_num}");
+                int option = random.Next(3);
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No Play");
+                        Console.WriteLine("The Position is " + Player_position);
+                        break;
+                    case 1:
+                        Console.WriteLine("Ladder");
+                        Player_position = Player_position + Die_num;
+                        Console.WriteLine("The Position is " + Player_position);
+                        break;
+                    case 2:
+                        Console.WriteLine("Snake");
+                        Player_position = Player_position - Die_num;
+                        Console.WriteLine("The Position is " + Player_position);
+                        break;
+                }
 
+                if (Player_position < 0)
+                {
+                    Player_position = 0;
+                    Console.WriteLine("Player's Position = " + Player_position);
+                }
+            }    
 
         }
     }
