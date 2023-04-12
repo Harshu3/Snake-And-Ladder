aec408b947361bc458249f4f60711afb247cbbed
Harshita <chandaliyaharshita222@gmail.com>
2023-04-12 14:42:45 +0530

    Adding Start Position

---
+++ Snake And Ladder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Snake_And_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int Player_position = 0;
+            Console.WriteLine("Position = " + Player_position);
+
+        }
+    }
+}
